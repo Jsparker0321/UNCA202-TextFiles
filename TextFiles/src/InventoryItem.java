f5a5782c69da4fd3f10d43f04e8aa6994b5a5a77
jessp <jessp@10.120.188.160>
2018-08-29 20:27:47 -0400

    Wrote classes "InventoryItem" and "Warehouse". Warehouse needs editing.

---
+++ TextFiles/src/InventoryItem.java
@@ -0,0 +1,46 @@
+
+public class InventoryItem {
+
+
+	private int sku;
+	private String disc;
+	private double price;
+	private int amount;
+
+	public InventoryItem(int sku, String disc, double price, int amount) {
+		this.sku = sku;
+		this.disc = disc;
+		this.price = price;
+		this.amount = amount;
+	}
+
+	public String toString() {
+		return sku + disc + price + amount;
+	}
+
+	public int getSku() {
+		return this.sku;
+	}
+
+	public String getDisc() {
+		return this.disc;
+	}
+
+	public double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
+
+
---
+++ TextFiles/src/Warehouse.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Warehouse {
+
+	public Warehouse() {
+		ArrayList<InventoryItem> wh = new ArrayList<>();
+	}
+	
+public void readInventoryFile() throws FileNotFoundException {
+	
+	
+	Scanner sc = new Scanner(new File("input.txt"));
+	
+	
+	
+}
+
+}
